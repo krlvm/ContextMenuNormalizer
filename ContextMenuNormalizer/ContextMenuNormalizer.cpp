a75d45f9a4ec63b7c3b0ac7af8b9555b7f996e8b
krlvm <51774833+krlvm@users.noreply.github.com>
2022-05-17 00:06:23 +0300

    Improve classic context menus appearance

--- ContextMenuNormalizer/ContextMenuNormalizer.cpp
+++ ContextMenuNormalizer/ContextMenuNormalizer.cpp
@@ -6,6 +6,10 @@
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "uxtheme.lib")
 
+#if _DEBUG
+#include <iostream>
+#endif
+
 #pragma region Pixel Color
 
 inline int PixClr(int val)
@@ -32,7 +36,105 @@ inline int PixA(BYTE* pPixel)
 
 #pragma endregion
 
-int RecolorizeBitmap(HBITMAP hbm)
+typedef void (*BitmapHandler)(int* r, int* g, int* b, int* a);
+
+#pragma region Recolorizators
+
+#define CONTEXT_MENU_BACKGROUND_LIGHT 238
+
+void Recolorize_Menu_PopupItem(int* r, int* g, int* b, int* a) {
+    if (*a == 13)
+    {
+        *a = 0;
+    }
+    else if (*r == 0 && *g == 56 && *b == 102)
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+        *a = 23;
+    }
+}
+void Recolorize_Menu_PopupGutter(int* r, int* g, int* b, int* a) {
+    *r = CONTEXT_MENU_BACKGROUND_LIGHT;
+    *g = CONTEXT_MENU_BACKGROUND_LIGHT;
+    *b = CONTEXT_MENU_BACKGROUND_LIGHT;
+    *a = 255;
+}
+void Recolorize_Menu_CheckBackground(int* r, int* g, int* b, int* a) {
+    *r = 0;
+    *g = 0;
+    *b = 0;
+    *a = 0;
+}
+void Recolorize_Menu_BarItem(int* r, int* g, int* b, int* a) {
+    if (*r == 0 && *g == 56 && *b == 102)
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+        *a = 28;
+    }
+    else if (*r == 0 && *g == 28 && *b == 51)
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+        *a = 20;
+    }
+    else if (*r == 0 && *g == 14 && *b == 26)
+    {
+        *r = 0;
+        *g = 0;
+        *b = 0;
+        *a = 12;
+    }
+}
+
+void Recolorize_ImmersiveMenu_PopupSeparator(int* r, int* g, int* b, int* a) {
+    *r = 215;
+    *g = 215;
+    *b = 215;
+}
+void Recolorize_ImmersiveMenu_PopupItem(int* r, int* g, int* b, int* a) {
+    if (*r == 255 && *g == 255 && *b == 255)
+    {
+        *r = 217;
+        *g = 217;
+        *b = 217;
+    }
+}
+void Recolorize_ImmersiveMenu_PopupBackground(int* r, int* g, int* b, int* a) {
+    *r = CONTEXT_MENU_BACKGROUND_LIGHT;
+    *g = CONTEXT_MENU_BACKGROUND_LIGHT;
+    *b = CONTEXT_MENU_BACKGROUND_LIGHT;
+}
+
+void Recolorize_ImmersiveMenuDark_PopupSeparator(int* r, int* g, int* b, int* a) {
+    if (*r == 128 && *g == 128 && *b == 128)
+    {
+        *r = 64;
+        *g = 64;
+        *b = 64;
+    }
+}
+void Recolorize_ImmersiveMenuDark_CheckBackground(int* r, int* g, int* b, int* a) {
+    //*r = 43;
+    //*g = 43;
+    //*b = 43;
+    //*a = 0;
+}
+void Recolorize_ImmersiveMenuDark_PopupBorders(int* r, int* g, int* b, int* a) {
+    if (*r == 128 && *g == 128 && *b == 128)
+    {
+        *r = 86;
+        *g = 86;
+        *b = 86;
+    }
+}
+#pragma endregion
+
+int RecolorizeBitmap(HBITMAP hbm, BitmapHandler handler)
 {
     BITMAP bm;
     GetObject(hbm, sizeof(bm), &bm);
@@ -54,20 +156,16 @@ int RecolorizeBitmap(HBITMAP hbm)
             int b = PixB(pPixel); // [0]
             int a = PixA(pPixel); // [3]
 
-            if (r == 238 && g == 238 && b == 238)
-            {
-                pPixel[2] = 242;
-                pPixel[1] = 242;
-                pPixel[0] = 242;
-            }
-            else if (r == 255 && g == 255 && b == 255)
-            {
-                pPixel[2] = 217;
-                pPixel[1] = 217;
-                pPixel[0] = 217;
-            }
+#if _DEBUG
+            _RPT1(0, "(%d, %d, %d / %d) \n", r, g, b, a);
+#endif
 
-            //pPixel[3] = 1;
+            handler(&r, &g, &b, &a);
+
+            pPixel[2] = r;
+            pPixel[1] = g;
+            pPixel[0] = b;
+            pPixel[3] = a;
 
             pPixel += 4;
         }
@@ -80,18 +178,36 @@ int RecolorizeBitmap(HBITMAP hbm)
     return TRUE;
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int NormalizeContextMenu(LPCWSTR pszClassList, int iPartId, BitmapHandler handler)
 {
     HBITMAP hBitmap;
 
-    HTHEME hTheme = OpenThemeData(GetForegroundWindow(), L"ImmersiveStart::Menu");
-    GetThemeBitmap(hTheme, 14, 0, TMT_DIBDATA, GBF_DIRECT, &hBitmap);
+    HTHEME hTheme = OpenThemeData(GetForegroundWindow(), pszClassList);
+    GetThemeBitmap(hTheme, iPartId, 0, TMT_DIBDATA, GBF_DIRECT, &hBitmap);
     CloseThemeData(hTheme);
 
-    RecolorizeBitmap(hBitmap);
+    return RecolorizeBitmap(hBitmap, handler);
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    NormalizeContextMenu(L"Menu", 14, Recolorize_Menu_PopupItem);
+    NormalizeContextMenu(L"Menu", 13, Recolorize_Menu_PopupGutter);
+    NormalizeContextMenu(L"Menu", 12, Recolorize_Menu_CheckBackground);
+    NormalizeContextMenu(L"Menu", 8, Recolorize_Menu_BarItem);
+    
+    NormalizeContextMenu(L"ImmersiveStart::Menu", 15, Recolorize_ImmersiveMenu_PopupSeparator);
+    NormalizeContextMenu(L"ImmersiveStart::Menu", 14, Recolorize_ImmersiveMenu_PopupItem);
+    NormalizeContextMenu(L"ImmersiveStart::Menu", 9, Recolorize_ImmersiveMenu_PopupBackground);
+    
+    NormalizeContextMenu(L"ImmersiveStartDark::Menu", 15, Recolorize_ImmersiveMenuDark_PopupSeparator);
+    NormalizeContextMenu(L"DarkMode_ImmersiveStart::Menu", 15, Recolorize_ImmersiveMenuDark_PopupSeparator);
+    NormalizeContextMenu(L"DarkMode::Menu", 15, Recolorize_ImmersiveMenuDark_PopupSeparator);
+    NormalizeContextMenu(L"DarkMode::Menu", 12, Recolorize_ImmersiveMenuDark_CheckBackground);
+    NormalizeContextMenu(L"DarkMode::Menu", 10, Recolorize_ImmersiveMenuDark_PopupBorders);
 
     return 0;
 }
